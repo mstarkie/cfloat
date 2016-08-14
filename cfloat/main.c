811ce73a9ac1faa0676240909a873141406f7877
Mike Starkie <hikemike@gmail.com>
2016-08-14 10:31:34 -0400

    Initial Commit

---
+++ cfloat/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cfloat
+//
+//  Created by Michael Starkie on 8/14/16.
+//  Copyright © 2016 Global Forge, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
